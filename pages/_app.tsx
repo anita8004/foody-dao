88f1989205d1e0e4519424d94061471cb5bc9a8d
anita <anita@fillygaming888.com>
2022-10-24 15:50:26 +0800

    Add rainbowkit

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
